b614e224fd3feb76e38f4c167f0d16b9de178c9b
Viola <101067663+just-purple@users.noreply.github.com>
2023-07-10 16:06:51 +0200

    Project Setup and Igniting The Web Server (#5)

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # shorty
-URL shortner
+URL shortner in Go programming language and using Redis as store mechanism for super fast data retrieval in the implementation
---
+++ main/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// json response
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hey Go URL Shortener !🚀",
+		})
+	})
+
+	err := r.Run(":9808")
+	if err != nil {
+		panic(fmt.Sprintf("Failed to start the web server - Error: %v", err))
+	}
+}
